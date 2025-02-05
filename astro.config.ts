5d742fe93b4c6664da8a6041f86eaf135d93249b
afetmin <kingpenguins@163.com>
2025-02-05 20:59:52 +0800

    更新 Astro 配置，移除 MDX 插件支持；新增 React.memo 和 React.lazy 相关博客文章

--- astro.config.ts
+++ astro.config.ts
@@ -6,16 +6,15 @@ import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 import { SITE } from "./src/config";
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react(), sitemap({
-    filter: page => SITE.showArchives || !page.endsWith("/archives"),
-  }), mdx()],
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    react(),
+    sitemap({ filter: page => SITE.showArchives || !page.endsWith("/archives"), }),
+  ],
   markdown: {
     remarkPlugins: [
       // [remarkToc, { heading: '目录' }],
